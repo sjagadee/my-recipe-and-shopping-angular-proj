63c7bd9f070d10f2eb800a27479d295d66d759a2
sjagadee <sjagadee@hawk.iit.edu>
2018-04-15 19:20:15 -0500

    Adding recipe details

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Component({
     selector: "app-recipes",
     templateUrl: "./recipes.component.html"
 })
 
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
 
+    recipes: Recipe[] = [
+        new Recipe("A Test Recipe", "This is simply a recipe", "https://i.pinimg.com/originals/dc/ae/62/dcae62410dc554f5fcfff458c8ebeaca.png"),
+        new Recipe("A Test Recipe", "This is simply a recipe", "https://i.pinimg.com/originals/dc/ae/62/dcae62410dc554f5fcfff458c8ebeaca.png")
+    ];
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+    }
 }
